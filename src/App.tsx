0ac768045518d7897b162991a814bee0ca573ce9
Adriano Souza <148394428+Dev-Adrianoo@users.noreply.github.com>
2025-01-09 14:35:48 -0300

    mudanças no projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,70 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+// interface equipeProps {
+//   name: string,
+//   cargo: string,
+//   age: string
+// }
 
+
+interface SocialMediaProps {
+  Youtube?: string
+  TikTok?: string
+  Instagram?: string
+  Linkedin?: string
+}
+interface AboutProps extends SocialMediaProps {
+  name: string
+  cargo: string
+  age: string
+}
+
+const SocialMedia = (props: SocialMediaProps) => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <a href={props.Youtube}>Youtube</a>
+      <hr />
+      <a>Tiktok {props.TikTok}</a>
+      <hr />
+      <a>Instagram {props.Instagram}</a>
+      <hr />
+      <a>Linkedin {props.Linkedin}</a>
+    </div>
   )
 }
 
-export default App
+const About = (props: AboutProps) => {
+  return (
+    <div>
+      <h2>Meu nome é: {props.name}</h2>
+      <h2>Tenho: {props.age} Anos</h2>
+      <h2>Cargo: {props.cargo}</h2>
+      <SocialMedia 
+      Youtube={props.Youtube}
+      Instagram={props.Instagram}
+      Linkedin={props.Linkedin}
+      TikTok={props.TikTok}
+      />
+      <hr></hr>
+    </div>
+  )
+}
+
+const Equipe = () => {
+  return (
+    <div>
+    <About name="Adriano" age="19" cargo="desenvolvedor" Youtube="https://google.com" />
+    <SocialMedia />
+    <About name="Hilary" age="17" cargo="Psicologa"  Instagram="https://Hilary" />
+    <About name="Erick" age="20" cargo="Empreendedor" Instagram="https://Erick" />
+    </div>
+  )
+}
+
+export default function App(){
+  return(
+    <div>
+      <h1>Conheça nossa equipe:</h1>
+      <Equipe />
+    </div>
+  )
+};
