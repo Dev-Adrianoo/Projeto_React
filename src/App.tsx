49fbb37dc5d2faa8045be7737d1d163c9aac3c4d
Adriano <148394428+Dev-Adrianoo@users.noreply.github.com>
2025-01-15 16:36:01 -0300

    usando eventChange

--- src/App.tsx
+++ src/App.tsx
@@ -1,65 +1,50 @@
 import React, { Component } from "react";
 
-interface socialProps {
-  instagram?: string
-  Tiktok?: string
-}
-interface EquipeProps extends socialProps{
-  nome: string
-  cargo: string
-  idade: string
+interface AppProps {
+  email: string
+  senha: string
 }
 
+interface AppState extends AppProps {
+  email: string
+  senha: string
+}
 
+class App extends Component<AppState, AppProps>
+{
 
-class Equipe extends Component <EquipeProps> {
+  constructor(props: AppProps){
+    super(props)
+    this.state = {
+      email: 'teste@teste.com',
+      senha: '',
+    }
+    this.TrocaEmail = this.TrocaEmail.bind(this)
+   }
+   TrocaEmail = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({email: event.target.value});
+  }
+
   render(): React.ReactNode {
     return (
-      <div>
-      <Sobre 
-      nome={this.props.nome}
-      cargo={this.props.cargo}
-      idade={this.props.idade}
-      />
-      <Social 
-      instagram={this.props.instagram}
-      Tiktok={this.props.Tiktok}
-      />
-      </div>
+      <main>
+        <h1>Login</h1>
+        Email:
+        <input
+        type="email"
+         name="email" 
+         id="" 
+         value={this.state.email}
+        onChange={this.TrocaEmail}/> <br />
+        Senha:
+        <input 
+        type="password"
+         name="password" 
+         id="" 
+         value={this.state.senha}/>
+      </main>
     )
   }
 }
 
-class Sobre extends Component <EquipeProps>{
-  render(): React.ReactNode {
-    return (
-      <div>
-      <h2>Olá sou o(a): {this.props.nome}</h2>
-      <h3>Profissão: {this.props.cargo} </h3>
-      <h3>idade: {this.props.idade} anos </h3>
-      </div>
-    )
-  }
-}
-
-const Social = (props: socialProps): JSX.Element => {
-  return(
-    <div>
-      <h3><a href="https://instagram.com.br">Instagram {props.instagram}</a></h3>
-      <h3><a href="https://www.tiktok.com/pt-BR/">Tik Tok {props.Tiktok}</a></h3>
-      <hr />
-    </div>
-  )
-}
-
-function App(){
-  return(
-    <div>
-      <h1>Conheça nossa equipe:</h1>
-      <Equipe nome="Adriano" cargo="Desenvolvedor" idade="24" />
-      <Equipe nome="Hilary" cargo="Psicologa" idade="17" />
-    </div>
-  )
-};
-
-export default App;
+export default App
